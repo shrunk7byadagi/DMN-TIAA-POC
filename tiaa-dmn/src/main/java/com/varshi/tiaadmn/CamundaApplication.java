6ff0c4b38d1230813c98857f2020c5979f0c0dc6
Shreyank Byadagi <shreyank@Shreyanks-MBP-2.lan>
2023-04-03 17:54:17 -0400

    adding dmn project

---
+++ tiaa-dmn/src/main/java/com/varshi/tiaadmn/CamundaApplication.java
@@ -0,0 +1,15 @@
+package com.varshi.tiaadmn;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("tiaa-dmn")
+public class CamundaApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+}
---
+++ tiaa-dmn/src/main/java/com/varshi/tiaadmn/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.varshi.tiaadmn;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "tiaa-dmn"; // BPMN Process ID
+
+}
